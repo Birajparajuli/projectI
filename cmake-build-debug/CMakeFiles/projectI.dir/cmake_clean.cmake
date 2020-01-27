1c7e21a50527b180b2d65961351cb87a968c6cbc
Birajparajuli <37270486+Birajparajuli@users.noreply.github.com>
2020-01-27 17:46:09 +0545

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/projectI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectI.dir/main.c.obj"
+  "libprojectI.dll.a"
+  "projectI.exe"
+  "projectI.exe.manifest"
+  "projectI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projectI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,14 @@
+/*2020 JAN 23*/
+/*PROJECT I*/
+/*This is main file of the project */
+/*This program is developed by Ashish, Saurav and Biraj as College Project*/
+
+/* HOTEL RESERVATION SYSTEM*/
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello Boys");
+    return 0;
+}
