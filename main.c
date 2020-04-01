974d17e2a4bfc0d15eb54e3ecdae5b0132c926c8
Birajparajuli <37270486+Birajparajuli@users.noreply.github.com>
2020-04-01 13:45:14 +0545

    Main selection menu added
    Main selection menu added for the logged user . From that menu user can cheoose different topic like room booking, see booked rooms. in the next update delete booking and edit booking will be added.

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <conio.h>
 #include <time.h>
+#include <windows.h>
 
 /*Local Libraries*/
 #include "login.h"
@@ -15,22 +16,72 @@
 #include "printBooking.h"
 
 
-
-
-
 int main() {
-    /*welcome screen*/
+    /*Welcome Screen*/
     welcome();
-
-    /*User Authentic System*/
+    /*Login Form*/
     login();
+    /*selection Menu*/
+    char choice;
+    int i;
+    while (1) {
+        printf("\n");
+        printf("\t\t|------------------ |MAIN MENU|-----------------|\n");
+        printf("\t\t_________________________________________________\n");
+        printf("\t\t|                                               |\n");
+        printf("\t\t|-------Please enter your choice for menu-------|");
+        printf("\n\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t| Enter 1 |         Book a room                 |\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t| Enter 2 |      View Customer Record           |\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t| Enter 3 |     Delete Customer Record          |\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t| Enter 4 |    Search Customer Record           |\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t| Enter 5 |        Edit Record                  |\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t| Enter 6 |            Exit                     |\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t|_______________________________________________|\n\n\n");
+        printf("\n");
+        printf("\t\t|-----------------------------------------------|\n");
+        printf("\t\t\tDate & Time: %s", ctime(&t));
+        printf("\t\t|-----------------------------------------------|\n");
 
-    /*Room Booking */
-    roomBooking();
-
-    /*Print Booking*/
-    printBooking();
-
-
-return 0;
+        choice = getche();
+        choice = toupper(choice);
+        switch (choice)           // SWITCH STATEMENT
+        {
+            case '1':
+                roomBooking();
+                break;
+            case '2':
+                printBooking();
+                break;
+            /*case '3':
+                ();
+                break;
+            case '4':
+                search();
+                break;
+            case '5':
+                edit();
+                break;
+            case '6':
+                system("cls");
+                printf("\n\n\t *****THANK YOU*****");
+                printf("\n\t FOR TRUSTING OUR SERVICE");
+                //	Sleep(2000);
+                exit(0);
+                break;*/
+            default:
+                system("cls");
+                printf("Incorrect Input");
+                printf("\n Press any key to continue");
+                getch();
+        }
+    }
+    return 0;
 }
--- printBooking.c
+++ printBooking.c
@@ -39,4 +39,3 @@ void printBooking(){
     fclose(f);
     getch();
 }
-f
